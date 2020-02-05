03d44a7e40f2d729f9f6567eecce1bbeb10931ed
Dalton Walker <daltonwalkerdw@yahoo.com>
2020-02-04 22:03:09 -0500

    New branch, added in login, register and child form functionality

--- gigapet/src/Components/Register.js
+++ gigapet/src/Components/Register.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {slideInDown} from 'react-animations';
 import styled, { keyframes } from 'styled-components';
 import NavBeforeLog from './NavBeforeLog';
@@ -7,10 +7,37 @@ import Bread from './img/Bread.png';
 import Carrot from './img/Carrot.png';
 import Broccoli from './img/Broccoli.png';
 import logo from './img/logo.png';
+import axios from "axios"
 
-function Register(){
+function Register(props){
  
     const DropDown = styled.div`animation: 10s ${keyframes `${slideInDown}`}`;
+
+    const [registerCredentials, setRegisterCredentials] = useState({
+        name: '',
+        email: '',
+        password: ''
+    })
+
+    const handleChange = (e) => {
+        setRegisterCredentials({
+            ...registerCredentials,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = (e) => {
+      e.preventDefault()
+       
+      
+      axios
+      .post('https://gigapetfive.herokuapp.com/auth/register', registerCredentials)
+      .then(res => {
+          console.log(res)
+          props.history.replace('/')
+      })
+      .catch(error => console.log(error))
+    }
 
     return(
 
@@ -33,12 +60,12 @@ function Register(){
             <DropDown><img className="tooBigAt1300px" src={Bread} alt='Bread' width="100px" postion="absolute"/></DropDown>
             <DropDown><img className="tooBigAt1100px" src={Broccoli} alt='Broccoli' width="100px" postion="absolute"/></DropDown>
         </div>
-<form>
+<form onSubmit={handleSubmit}>
     <div className="RegForm">
         <div className="FormStyle">
-            <p>Name:</p> <input type="text" placeholder="Enter a name"></input>
-            <p>E-mail:</p> <input type="email" placeholder="Enter your e-mail"></input>
-            <p>Password:</p> <input type="password" placeholder="Enter your password"></input>
+            <p>Name:</p> <input type="text" placeholder="Enter a name" name="name" value={registerCredentials.name} onChange={handleChange}></input>
+            <p>E-mail:</p> <input type="email" placeholder="Enter your e-mail" name="email" value={registerCredentials.email} onChange={handleChange}></input>
+            <p>Password:</p> <input type="password" placeholder="Enter your password" name="password" value={registerCredentials.password} onChange={handleChange}></input>
             <button className="btn">Submit</button>
         </div>
     </div>
