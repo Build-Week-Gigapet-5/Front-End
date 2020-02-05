8b8fb764fe6b545be9d6358f4fa3af6d6ee3d57e
Sonica1031 <Sonica1031@hotmail.com>
2020-02-04 18:05:00 -0800

    Working everything

--- gigapet/src/Components/YearSearch.js
+++ gigapet/src/Components/YearSearch.js
@@ -24,12 +24,11 @@ useEffect(()=> {
             <div>
 
                 <select onChange={Change} placeholder="Pick a year">
-                <option value="2015">2015</option>
-                <option value="2016">2016</option>
-                <option value="2017">2017</option>
-                <option value="2018">2018</option>
-                <option value="2019">2019</option>
-                <option value="2020">2020</option>             
+                <option value="2015">2016</option>
+                <option value="2016">2017</option>
+                <option value="2017">2018</option>
+                <option value="2018">2019</option>
+                <option value="2019">2020</option>            
             </select>
             {searchAnswer.map(food =>{
                 return(
